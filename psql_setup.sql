29f4dc83c61b9409119bad5a11ad3a489e075eec
Sanja Kostic <147453284+sanjakostic@users.noreply.github.com>
2024-02-11 17:28:56 -0800

    EDR Diagram and Setup Code

---
+++ psql_setup.sql
@@ -0,0 +1,67 @@
+﻿CREATE TABLE "departments" (
+    "dept_no" VARCHAR(4)   NOT NULL,
+    "dept_name" VARCHAR(200)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(5)   NOT NULL,
+    "title" VARCHAR(200)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "Employees" (
+    "emp_no" INT   NOT NULL,
+    "emp_title_id" VARCHAR(5)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(100)   NOT NULL,
+    "last_name" VARCHAR(100)   NOT NULL,
+    "sex" VARCHAR(1)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     ),
+    CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+    REFERENCES "titles" ("title_id")
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no",
+        "dept_no"
+     ),
+    CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+    REFERENCES "Employees" ("emp_no"),
+    CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+    REFERENCES "departments" ("dept_no")
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR(4)   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no",
+        "emp_no"
+     ),
+    CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+    REFERENCES "departments" ("dept_no"),
+    CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+    REFERENCES "Employees" ("emp_no")
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no",
+        "salary"
+     ),
+    CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+    REFERENCES "Employees" ("emp_no")
+);
